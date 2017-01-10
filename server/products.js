76a8b4608de2bdf354f6f56a4a884777c08b5b19
Colin Jaffe <abbreviatedman@Colins-MacBook-Pro.local>
2017-01-10 17:22:00 -0500

    Some early work on routes

---
+++ server/products.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const db = require('APP/db');
+const Product = db.model('products');
+
+const {mustBeLoggedIn, forbidden} = require('./auth.filters');
+
+module.exports = require('express').Router()
+  .get('/', (req, res, next) =>
+    Product.findAll()
+    .then(products => res.json(products))
+    .catch(next))
+  .post('/', forbidden('only admins can create products'), (req, res, next) =>
+    Product.create(req.body)
+    .then(product => res.status(201).json(product))
+    .catch(next))
+  .get('/:id', (req, res, next) =>
+    Product.findById(req.params.id)
+    .then(product => res.json(product))
+    .catch(next));
