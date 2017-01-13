f47bc5a3d7cbd33ef46577d243c769fbd175cb23
Richard Shyong <rshyong@gmail.com>
2017-01-12 19:07:23 -0500

    productContainer tests

--- app/containers/ProductContainer.test.jsx
+++ app/containers/ProductContainer.test.jsx
@@ -7,13 +7,24 @@ import ProductContainer from './ProductContainer';
 describe('Product container', () => {
   let productContainer;
   beforeEach('Create Product', () => {
-    let sample = {
-      productId: 1
-    }
     productContainer = shallow(<ProductContainer routeParams={{productId: 1}} />);
   });
 
-  it('should render div', () => {
+  it('should render a div', () => {
     expect(productContainer.is('div')).to.be.equal(true);
-  })
+  });
+
+  it('should render the correct nodes', () => {
+    expect(productContainer.get(0).props.children[0].props.children[0]).to.contain('Category');
+    expect(productContainer.get(0).props.children[4].props.children[0]).to.contain('Price');
+    expect(productContainer.get(0).props.children[5].props.children[0]).to.contain('Inventory');
+    expect(productContainer.get(0).props.children[6].props.children[0]).to.contain('Calories');
+    expect(productContainer.get(0).props.children[7].props.children[0]).to.contain('Sugar');
+    expect(productContainer.get(0).props.children[8].props.children[0]).to.contain('Fiber');
+    expect(productContainer.get(0).props.children[9].props.children[0]).to.contain('Protein');
+    expect(productContainer.get(0).props.children[10].props.children[0]).to.contain('Average User Rating');
+  });
+
+
+
 });
