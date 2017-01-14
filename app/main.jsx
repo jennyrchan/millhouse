f71153a38f303ba7daee48118958e47a61a96837
Richard Shyong <rshyong@gmail.com>
2017-01-14 14:57:36 -0500

    not working

--- app/main.jsx
+++ app/main.jsx
@@ -5,17 +5,21 @@ import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
 import store from './store'
-import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import Navbar from './components/Navbar'
-import ProductContainer from './containers/ProductContainer'
+import Product from './components/Product'
 
 import {fetchCart} from './reducers/cart';
+import {fetchProduct, fetchReviews} from './reducers/product';
 
 const onAppEnter = () => {
   fetchCart();
 };
+
+const onProductEnter = (route) => (store.dispatch(fetchProduct(+route.params.productId)));
+
+const onReviewsEnter = (route) => (fetchReviews(+route.params.productId));
 
 const AuthContainer = connect(
   ({ auth }) => ({ user: auth })
@@ -32,7 +36,7 @@ render (
     <Router history={browserHistory}>
       <Route path="/" component={AuthContainer} onEnter={onAppEnter}>
         <IndexRedirect to="/products/:productId" />
-        <Route path="/products/:productId" component={ProductContainer} />
+        <Route path="/products/:productId" onEnter = {onProductEnter} component={Product} />
       </Route>
     </Router>
   </Provider>,
