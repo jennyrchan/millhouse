88542ba2cf5a0ed0d73d11e7476ef6c48c3de33b
Jenny Chan <jrc337@cornell.edu>
2017-01-16 12:08:09 -0500

    edited css

--- app/components/UserSidebar.jsx
+++ app/components/UserSidebar.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
 const UserSidebar = props => {
     return (
       <sidebar>
         <section>
-          <Link to = {`/users/${props.userId}/orders`} > Orders </Link>
+          <Link to={`/users/${props.userId}/orders`}> Orders </Link>
         </section>
 
         <section>
-          <Link to={`/users/${props.userId}/reviews`} > Reviews </Link>
+          <Link to={`/users/${props.userId}/reviews`}> Reviews </Link>
         </section>
 
         <section>
           <Link to={`/users/${props.userId}/settings`}> Settings </Link>
         </section>
 
-        <div className = 'millhouse' />
-
+        <div className="millhouse" />
       </sidebar>
     )
-
 }
 
 export default UserSidebar;
-
--- app/main.jsx
+++ app/main.jsx
@@ -78,11 +78,11 @@ render(
       <Route path="/" component={AuthContainer} onEnter={onAppEnter}>
         <IndexRedirect to="/products" />
         <Route path="/products" component={Products} />
-        <Route path="/products/:productId" onEnter = {onProductEnter} component={Product} />
+        <Route path="/products/:productId" onEnter={onProductEnter} component={Product} />
         <Redirect from="/users/:userId" to="/users/:userId/orders" />
-        <Route path="/users/:userId/orders" component= {UserOrders}onEnter = {onUserOrdersEnter}/>
-        <Route path="/users/:userId/reviews" component= {UserReviews} onEnter = {onUserReviewsEnter}/>
-        <Route path="/users/:userId/settings" component= {UserSettings} onEnter = {onUserSettingsEnter} />
+        <Route path="/users/:userId/orders" component={UserOrders} onEnter={onUserOrdersEnter} />
+        <Route path="/users/:userId/reviews" component={UserReviews} onEnter={onUserReviewsEnter} />
+        <Route path="/users/:userId/settings" component={UserSettings} onEnter={onUserSettingsEnter} />
       </Route>
     </Router>
   </Provider>,
