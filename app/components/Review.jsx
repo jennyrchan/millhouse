79018c140cb72fcbc375a8c7e10c6c4ddb8789fe
Jenny Chan <jrc337@cornell.edu>
2017-01-16 11:15:07 -0500

    added background and simpsons font

--- app/components/Review.jsx
+++ app/components/Review.jsx
@@ -4,11 +4,11 @@ function Review (props) {
 
   let arr = [];
   for (let i = 0; i < props.rating; i++) {
-    arr.push(<img src={`/cheerio.jpg`} key={i} />);
+    arr.push(<img className="img-circle" src={`/cheerio.jpg`} key={i} />);
   }
 
   return (
-    <div>
+    <div id="review">
       <h2>{ props.title }</h2>
       <p>{ props.body }</p>
       <h3>User Rating: {arr}</h3>
